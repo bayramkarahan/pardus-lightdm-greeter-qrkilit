63b3a4aecd62ec7c62d8b2482a8fdd16577cb23b
karahan <bayramk@gmail.com>
2024-10-07 08:48:31 +0300

    test

--- module/qrkilit.py
+++ module/qrkilit.py
@@ -2,38 +2,38 @@ import asyncio
 import qrcode
 import random
 
-ekilitpopover = None
-ekilitimage = None
-ekilittext = None
-ekilitfile ="/tmp/qrekillit.png"
-ekilitinput= None
+qrkilitpopover = None
+qrkilitimage = None
+qrkilittext = None
+qrkilitfile ="/tmp/qrekillit.png"
+qrkilitinput= None
 random_label_text=""
 _last_random_label_text=""
 result_pass_str=""
 
-def _ekilitqrkod_button_event(widget=None):
+def _qrkilitqrkod_button_event(widget=None):
     global random_label_text
     random_label_text=_("Loading...")
     #loginwindow.o("ui_popover_network").popup()
     os.system("rm /tmp/qrekillit.png")
-    ekilitpopover.popup()
-    ekilitqrcode_control_event()
+    qrkilitpopover.popup()
+    qrkilitqrcode_control_event()
 
 
-def update_popover_ekilit_text():
+def update_popover_qrkilit_text():
     global _last_random_label_text
     global result_pass_str
     if _last_random_label_text != random_label_text:
         _last_random_label_text = random_label_text
-        ekilittext.set_text("ekilit sistemi")
+        qrkilittext.set_text("qrkilit sistemi")
         img = qrcode.make(random_label_text)
-        #ekilittext.set_text(lan_ip.strip())
-        img.save(ekilitfile)
-        ekilitimage.set_from_file(ekilitfile)       
-    GLib.timeout_add(500,update_popover_ekilit_text)
+        #qrkilittext.set_text(lan_ip.strip())
+        img.save(qrkilitfile)
+        qrkilitimage.set_from_file(qrkilitfile)       
+    GLib.timeout_add(500,update_popover_qrkilit_text)
 
 @asynchronous
-def ekilitqrcode_control_event():
+def qrkilitqrcode_control_event():
     global random_label_text
     global result_pass_str
     random_label_text=str(random.randrange(100000, 999999))
@@ -53,43 +53,43 @@ def ekilitqrcode_control_event():
     p6=str(abs(((int(p6)+int(p1))-int(p2))*int(p3)))
     result_pass_str=str(p1[0]+p2[0]+p3[0]+p4[0]+p5[0]+p6[0])
 
-def loginekilitbutton_clicked(button):
-    global ekilitpopover
+def loginqrkilitbutton_clicked(button):
+    global qrkilitpopover
     global result_pass_str
     
-    if ekilitinput.get_text() == result_pass_str:
+    if qrkilitinput.get_text() == result_pass_str:
         #print(random_label_text)
-        ekilitpopover.hide()
+        qrkilitpopover.hide()
         print("giriş yapılıyor..")
         os.system("echo 'ebaqrebaqr:ebaqr:ebaqr' | netcat localhost 7777 &")
     
 
 def module_init():
-    global ekilitpopover
-    global ekilitimage
-    global ekilittext
-    global ekilitinput
-    global loginekilitbutton
-    ekilitpopover = Gtk.Popover()
-    ekilitimage = Gtk.Image()
-    ekilittext = Gtk.Label()
-    ekilitinput=Gtk.Entry()
-    loginekilitbutton = Gtk.Button.new_with_label("Giriş")
-    loginekilitbutton.connect("clicked",loginekilitbutton_clicked)
+    global qrkilitpopover
+    global qrkilitimage
+    global qrkilittext
+    global qrkilitinput
+    global loginqrkilitbutton
+    qrkilitpopover = Gtk.Popover()
+    qrkilitimage = Gtk.Image()
+    qrkilittext = Gtk.Label()
+    qrkilitinput=Gtk.Entry()
+    loginqrkilitbutton = Gtk.Button.new_with_label("Giriş")
+    loginqrkilitbutton.connect("clicked",loginqrkilitbutton_clicked)
     b = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-    b.pack_start(ekilitimage,False,False,0)
+    b.pack_start(qrkilitimage,False,False,0)
     
-    b.pack_start(ekilittext,False,False,0)
-    b.pack_start(ekilitinput,False,False,0)
-    b.pack_start(loginekilitbutton,False,False,0)
+    b.pack_start(qrkilittext,False,False,0)
+    b.pack_start(qrkilitinput,False,False,0)
+    b.pack_start(loginqrkilitbutton,False,False,0)
 
-    ekilitpopover.add(b)
-    ekilitpopover.set_position(Gtk.PositionType.BOTTOM)
-    button = Gtk.MenuButton(label="E-KİLİT", popover=ekilitpopover)
-    button.connect("clicked",_ekilitqrkod_button_event)
+    qrkilitpopover.add(b)
+    qrkilitpopover.set_position(Gtk.PositionType.BOTTOM)
+    button = Gtk.MenuButton(label="QRKILIT", popover=qrkilitpopover)
+    button.connect("clicked",_qrkilitqrkod_button_event)
     loginwindow.o("ui_box_bottom_left").pack_end(button, False, True, 10)
     button.get_style_context().add_class("icon")
     button.show_all()
     b.show_all()
-    #loginwindow.o("ui_button_network").connect("clicked",_ekilitqrkod_button_event)
-    update_popover_ekilit_text()
+    #loginwindow.o("ui_button_network").connect("clicked",_qrkilitqrkod_button_event)
+    update_popover_qrkilit_text()
