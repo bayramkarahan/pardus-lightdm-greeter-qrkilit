fd9d72d623dfb05141e71146ea5ae46a77fa871f
karahan <bayramk@gmail.com>
2024-11-15 23:39:37 +0300

    test

--- module/qrkilit.py
+++ module/qrkilit.py
@@ -51,7 +51,7 @@ def get_hotp_token(secret, intervals_no):
 
 def get_totp_token(secret):
     #ensuring to give the same otp for 30 seconds
-    x =str(get_hotp_token(secret,intervals_no=int(time.time())//30))
+    x =str(get_hotp_token(secret,intervals_no=int(time.time())//60))
     #adding 0 in the beginning till OTP has 6 digits
     while len(x)!=6:
         x+='0'
